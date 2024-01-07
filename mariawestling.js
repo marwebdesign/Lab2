4b88c44becd53c1fe881f48634686b7b67649443
marwebdesign <maria_westling@hotmail.com>
2024-01-07 21:52:18 +0100

    alot of code, mainly commented out, working on eventlisteners and pushing to array

--- mariawestling.js
+++ mariawestling.js
@@ -3,42 +3,83 @@ const welcomeParagraph = document.getElementById("welcomeParagraph")
 const firstName = document.getElementById("firstName")
 const lastName = document.getElementById("lastName")
 const quizContentDiv = document.getElementById("quizContentDiv")
-let openQvalue = document.getElementById("openQ").value
+//let openQvalue = document.getElementById("openQ").value
 const quizForm = document.getElementById("quizForm")
 const resultsDiv = document.getElementById("resultsDiv")
 const userinformationDiv = document.getElementById("userInformationDiv")
+let correctAnswers = document.getElementById("correctAnswers")
+let blue = document.getElementById("blue")
+let green = document.getElementById("green")
+let red = document.getElementById("red")
+const q1 = document.querySelector('input[value="Rachel"]:checked')
+const rachel = document.getElementById("rachel")
+let street = document.getElementById("q1")
+let howMany = document.getElementById("howMany")
+let firstQAnswer = document.getElementById("firstQAnswer")
+let secondQAnswer = document.getElementById("secondQAnswer")
+const q2 = document.getElementById("q2")
 
-userForm.addEventListener("submit", function (event) {
-  event.preventDefault()
-  const firstNameInput = firstName.value
-  const lastNameInput = lastName.value
-  welcomeParagraph.textContent = `Welcome ${firstNameInput} ${lastNameInput}. Let's start the quiz!`
-  quizContentDiv.style.display = "block"
-  userinformationDiv.style.display = "none"
-})
+// userForm.addEventListener("submit", function (event) {
+//   event.preventDefault()
+//   const firstNameInput = firstName.value
+//   const lastNameInput = lastName.value
+//   welcomeParagraph.textContent = `Welcome ${firstNameInput} ${lastNameInput}. Let's start the quiz!`
+//   quizContentDiv.style.display = "block"
+//   userinformationDiv.style.display = "none"
+// })
 
-quizForm.addEventListener("submit", function (event) {
-  event.preventDefault()
-  resultsDiv.style.display = "block"
-})
+// quizForm.addEventListener("submit", function (event) {
+//   event.preventDefault()
+//   resultsDiv.style.display = "block"
+//   if (blue.checked === true) {
+//     correctAnswers.textContent = "blue!"
+//     console.log("gio")
+//   }
+// })
+
+const arr = []
+
+quizForm.addEventListener("click", function () {
+  if (rachel.checked) {
+    arr.push = ["rachel"]
+  firstQAnswer.textContent = "That's correct!"
+} else {
+         firstQAnswer.textContent = "Try again!"   
+}
+console.log(arr)
+    })
+
+q2.addEventListener("change", function() {
+if (q2.value === "python") {
+  arr.push = ["python"]
+  secondQAnswer.textContent = "That's correct!"
+} else {
+         secondQAnswer.textContent = "Try again!"   
+}
+console.log(arr)
+
+    })
 
 
-// function Func() {
-fetch("https://opentdb.com/api.php?amount=10")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => console.log(data));
-// }
 
 
 
-const apiWeather = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=07e49e158145921c4197d32487c9067e'
-fetch(apiWeather)
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) =>
-    console.log(data))
 
-console.log(openQvalue)
+
+//quizForm.addEventListener("submit", function (event) {
+ // event.preventDefault()
+ // let openQ = street.value
+  
+ // if (openQ.includes("street")) {
+//numberOfCorrectAnswers.push["street"]
+//  } else
+  
+// if (blue.checked === true)
+//    {
+// numberOfCorrectAnswers.push["blue"]
+//  } 
+  //      let total = numberOfCorrectAnswers.length
+ //   console.log(numberOfCorrectAnswers.length)
+  //    howMany.innerHTML = `you got ${total} out of 5!`
+
+// })
